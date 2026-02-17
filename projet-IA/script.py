027c50603a8674e5910627c888ef81b1447dbf3b
Melissamgd <melissa.mongondryy@gmail.com>
2026-02-17 11:01:51 +0000

    Script seance 3

---
+++ projet-IA/script.py
@@ -0,0 +1,19 @@
+# Liste des mois avec un fruit de saison
+mois_fruits = {
+    "Janvier": "Orange",
+    "Février": "Kiwi",
+    "Mars": "Pomme",
+    "Avril": "Fraise",
+    "Mai": "Cerise",
+    "Juin": "Abricot",
+    "Juillet": "Pêche",
+    "Août": "Melon",
+    "Septembre": "Raisin",
+    "Octobre": "Poire",
+    "Novembre": "Clémentine",
+    "Décembre": "Ananas"
+}
+
+# Affichage
+for mois, fruit in mois_fruits.items():
+    print(f"{mois} : {fruit}")
---
+++ projet-IA/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir projet2
+cd projet2
+mkdir css js
+touch index.html
+touch css/style.css
+touch js/script.js
